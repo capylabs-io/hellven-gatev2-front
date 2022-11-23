dacf889248600eb163ec2d6350deb478dd76993d
t-linh <linhtrinh.a8@gmail.com>
2022-11-23 16:06:22 +0900

    fix news

--- src/views/home/pages/News.vue
+++ src/views/home/pages/News.vue
@@ -1,9 +1,143 @@
 <template>
-  <div class="news d-lfex flex-column">
+  <div class="news d-lfex flex-column py-16">
     <div class="news-title">
       <v-img :src="require(`@/assets/news/new-title.webp`)"></v-img>
     </div>
-    <div class="news-card pt-10 item-center">
+    <v-row class="pt-10 ma-auto px-5 max-content">
+      <v-col cols="12" sm="6" lg="3">
+        <v-card class="new-card-boder" white>
+          <v-sheet
+            v-if="loading"
+            :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+          >
+            <v-skeleton-loader
+              class="mx-auto"
+              max-width="100%"
+              type="image"
+            ></v-skeleton-loader>
+          </v-sheet>
+          <v-img
+            v-else
+            class="mx-auto new-card-image"
+            :src="require(`@/assets/news/new1-image.webp`)"
+            max-width="100%"
+          />
+          <div class="pl-4 pt-4 bungee-font gray-text">
+            <p style="margin: 0">{{ $t("new.new1.subtitle") }}</p>
+          </div>
+          <v-card-title class="bungee-font" style="font-size: 23px">
+            {{ $t("new.new1.title") }}
+          </v-card-title>
+
+          <v-card-subtitle
+            class="text-break kanit-font pt-2"
+            style="font-size: 15px"
+          >
+            {{ $t("new.new1.link") }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" lg="3">
+        <v-card class="new-card-boder" white>
+          <v-sheet
+            v-if="loading"
+            :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+          >
+            <v-skeleton-loader
+              class="mx-auto"
+              max-width="100%"
+              type="image"
+            ></v-skeleton-loader>
+          </v-sheet>
+          <v-img
+            v-else
+            class="mx-auto new-card-image"
+            :src="require(`@/assets/news/new1-image.webp`)"
+            max-width="100%"
+          />
+          <div class="pl-4 pt-4 bungee-font gray-text">
+            <p style="margin: 0">{{ $t("new.new1.subtitle") }}</p>
+          </div>
+          <v-card-title class="bungee-font" style="font-size: 23px">
+            {{ $t("new.new1.title") }}
+          </v-card-title>
+
+          <v-card-subtitle
+            class="text-break kanit-font pt-2"
+            style="font-size: 15px"
+          >
+            {{ $t("new.new1.link") }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" lg="3">
+        <v-card class="new-card-boder" white>
+          <v-sheet
+            v-if="loading"
+            :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+          >
+            <v-skeleton-loader
+              class="mx-auto"
+              max-width="100%"
+              type="image"
+            ></v-skeleton-loader>
+          </v-sheet>
+          <v-img
+            v-else
+            class="mx-auto new-card-image"
+            :src="require(`@/assets/news/new1-image.webp`)"
+            max-width="100%"
+          />
+          <div class="pl-4 pt-4 bungee-font gray-text">
+            <p style="margin: 0">{{ $t("new.new1.subtitle") }}</p>
+          </div>
+          <v-card-title class="bungee-font" style="font-size: 23px">
+            {{ $t("new.new1.title") }}
+          </v-card-title>
+
+          <v-card-subtitle
+            class="text-break kanit-font pt-2"
+            style="font-size: 15px"
+          >
+            {{ $t("new.new1.link") }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" lg="3">
+        <v-card class="new-card-boder" white>
+          <v-sheet
+            v-if="loading"
+            :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+          >
+            <v-skeleton-loader
+              class="mx-auto"
+              max-width="100%"
+              type="image"
+            ></v-skeleton-loader>
+          </v-sheet>
+          <v-img
+            v-else
+            class="mx-auto new-card-image"
+            :src="require(`@/assets/news/new1-image.webp`)"
+            max-width="100%"
+          />
+          <div class="pl-4 pt-4 bungee-font gray-text">
+            <p style="margin: 0">{{ $t("new.new1.subtitle") }}</p>
+          </div>
+          <v-card-title class="bungee-font" style="font-size: 23px">
+            {{ $t("new.new1.title") }}
+          </v-card-title>
+
+          <v-card-subtitle
+            class="text-break kanit-font pt-2"
+            style="font-size: 15px"
+          >
+            {{ $t("new.new1.link") }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- <div class="news-card pt-10 item-center">
       <v-card class="new-card" white>
         <v-sheet
           v-if="loading"
@@ -130,7 +264,7 @@
           {{ $t("new.new3.link") }}
         </v-card-subtitle>
       </v-card>
-    </div>
+    </div> -->
     <div class="new-btn">
       <v-btn
         color="violet"
@@ -167,11 +301,19 @@ export default {
 </script>
 <style scoped>
 .news {
-  height: 770px;
+  height: auto;
   width: 100%;
   padding-top: 6%;
   position: relative;
   background: transparent;
+}
+.max-content {
+  max-width: 88%;
+}
+@media screen and (max-width: 960px) {
+  .max-content {
+    max-width: 100%;
+  }
 }
 .news-card {
   display: flex;
@@ -192,6 +334,12 @@ export default {
   box-shadow: 0px 11px 1px 0px #d9d9d9 !important;
   max-width: 23%;
   margin: 0 20px;
+}
+.new-card-boder {
+  border-radius: 16px;
+  box-shadow: 0px 11px 1px 0px #d9d9d9 !important;
+  /* max-width: 23%;
+  margin: 0 20px; */
 }
 .btn-customize {
   border-radius: 15px;
